c4eaa693b79e3bf43f0b4fa8c3e8be6946ed28e6
Tbaile <bailetti.tommaso@gmail.com>
2016-04-01 11:07:25 +0200

    Deleted old way to gather info, Jersey implemented

--- src/main/java/it/iubar/fatturapa/FatturaUtil.java
+++ src/main/java/it/iubar/fatturapa/FatturaUtil.java
@@ -1,11 +1,10 @@
 package it.iubar.fatturapa;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.net.URL;
-import java.net.URLConnection;
-
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+ 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -20,53 +19,38 @@ public class FatturaUtil {
 	public static final String RESPONSE_TAG = "response";
 	public static final String XML_TAG = "xml";
 	
-	private static String USER = "user";
-	private static String PASSWORD = "password";
-	
-	public static String getUser() {
-		return USER;
-	}
+	private static String getResponse(String url){
+		try {
+			Client client = Client.create();
+			WebResource webResource = client.resource(url);
+			ClientResponse response = webResource.accept("application/json").get(ClientResponse.class);
+			if (response.getStatus() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+			}
 
-	public static void setUser(String user) {
-		USER = user;
-	}
-
-	public static String getPassword() {
-		return PASSWORD;
-	}
-
-	public static void setPassword(String password) {
-		PASSWORD = password;
-	}
-
-	private static String getHttpString(String url) throws Exception{
-		URL website = new URL(url);
-        URLConnection connection = website.openConnection();
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-        StringBuilder response = new StringBuilder();
-        String inputLine;
-
-        while ((inputLine = in.readLine()) != null) 
-            response.append(inputLine);
-
-        in.close();
-
-        return response.toString();
+			 return response.getEntity(String.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
+	@Deprecated
+	//Non utile dopo Debug
 	public static String getXmlString(String url) throws Exception{
-		JSONObject jsonObject = new JSONObject(getHttpString(url));
+		JSONObject jsonObject = new JSONObject(FatturaUtil.getResponse(url));
 		JSONObject data = jsonObject.getJSONObject(DATA_TAG);
 		return data.getString(XML_TAG);
 	}
 	
 	public static Document getXmlDocument(String url) throws Exception{
 		
-		String data = FatturaUtil.getXmlString(url);
+		String data = FatturaUtil.getResponse(url);
 		
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 	    DocumentBuilder builder = factory.newDocumentBuilder();
 	   	return builder.parse(new InputSource(new StringReader(data)));
 	}
+
+
 }
