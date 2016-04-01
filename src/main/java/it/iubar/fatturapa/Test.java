3dfd2f54c6cdfa593c11db9caef010a4953e899f
Tbaile <bailetti.tommaso@gmail.com>
2016-04-01 18:17:19 +0200

    Small debug

--- src/main/java/it/iubar/fatturapa/Test.java
+++ src/main/java/it/iubar/fatturapa/Test.java
@@ -3,6 +3,7 @@ package it.iubar.fatturapa;
 public class Test {
 	public static void main(String[] args) throws Exception{
 		String url = "http://www.fatturatutto.it/app/api/test/fattura-esempio";
-		System.out.println(FatturaUtil.getXmlString(url));
+		FatturaUtil.setUser("andrea@iubar.it");
+		System.out.println(FatturaUtil.getInfo(url, FatturaUtil.XML_TAG));
 	}
 }
