877d7eb84e5ce100b4afd5a7e2b8149395bc5180
Tbaile <bailetti.tommaso@gmail.com>
2016-04-01 13:15:30 +0200

    Changes to FatturaUtils
    Added Request to url
    Added getPayload and getTimeStamp methods
    Changed getSignature to respect Base64 codec

--- src/main/java/it/iubar/fatturapa/FatturaUtil.java
+++ src/main/java/it/iubar/fatturapa/FatturaUtil.java
@@ -3,7 +3,12 @@ package it.iubar.fatturapa;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.io.StringReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
 import com.sun.jersey.api.client.Client;
@@ -18,6 +23,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
+import org.apache.commons.codec.binary.Base64;
 
 public class FatturaUtil {
 	
@@ -27,10 +33,59 @@ public class FatturaUtil {
 	public static final String XML_TAG = "xml";
 	
 	private static String API_KEY = setApi();
+	private static String USER = "user@user.it";
 	
+	public static String getUser() {
+		return USER;
+	}
+
+	public static void setUser(String user) {
+		USER = user;
+	}
+	
+	public static String getApi(){
+		return API_KEY;
+	}
+	
+	private static String setApi(){
+		Properties prop = new Properties();
+		InputStream input = null;
+		
+		try{
+			input = new FileInputStream("src/main/resources/apikey.ini");
+			prop.load(input);
+			return prop.getProperty("apikey");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return null;
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
 	private static String getResponse(String url){
 		try {
 			Client client = Client.create();
+			JSONObject js = new JSONObject();
+			
+			js.put("user", FatturaUtil.getUser());
+			js.put("time", getTimeStamp());
+			js.put("sig", FatturaUtil.getSignature());
+			
+			URL urlConnection = new URL(url);
+			URLConnection connection = urlConnection.openConnection();
+			connection.setDoOutput(true);
+			connection.setRequestProperty("Content-Type", "application/json");
+			OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+			out.write(js.toString());
+			out.close();
+		
 			WebResource webResource = client.resource(url);
 			ClientResponse response = webResource.accept("application/json").get(ClientResponse.class);
 			if (response.getStatus() != 200) {
@@ -59,55 +114,28 @@ public class FatturaUtil {
 	    DocumentBuilder builder = factory.newDocumentBuilder();
 	   	return builder.parse(new InputSource(new StringReader(data)));
 	}
-
-	public static String getSignature(String msg) {
-		String digest = null;
-		String algo = "HmacSHA256";
-		String keyString = FatturaUtil.getApi();
-	    try {
-	    	SecretKeySpec key = new SecretKeySpec((keyString).getBytes("UTF-8"), algo);
-	    	Mac mac = Mac.getInstance(algo);
-	    	mac.init(key);
-
-	    	byte[] bytes = mac.doFinal(msg.getBytes("ASCII"));
-
-	    	StringBuffer hash = new StringBuffer();
-	    	for (int i = 0; i < bytes.length; i++) {
-	    		String hex = Integer.toHexString(0xFF & bytes[i]);
-	    		if (hex.length() == 1) {
-	    			hash.append('0');
-	    		}
-	        	hash.append(hex);
-	      	}
-	      	digest = hash.toString();
-	    } catch (Exception e) {
-	    }
-	    return digest;
-	  }
 	
-	private static String setApi(){
-		Properties prop = new Properties();
-		InputStream input = null;
-		
-		try{
-			input = new FileInputStream("src/main/resources/apikey.ini");
-			prop.load(input);
-			return prop.getProperty("apikey");
-		} catch (IOException ex) {
-			ex.printStackTrace();
-			return null;
-		} finally {
-			if (input != null) {
-				try {
-					input.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
+	public static String getPayLoad(){
+		return FatturaUtil.getUser() + getTimeStamp() + FatturaUtil.getApi();
 	}
 	
-	public static String getApi(){
-		return API_KEY;
+	private static String getTimeStamp(){
+		return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").format(new Date());
+	}
+
+	public static String getSignature() {
+		String payload = FatturaUtil.getPayLoad();
+		String algo = "HmacSHA256";
+		String keyString = FatturaUtil.getApi();
+		try{
+			Mac sha256_HMAC = Mac.getInstance(algo);
+			SecretKeySpec secret_key = new SecretKeySpec(keyString.getBytes(), algo);
+			sha256_HMAC.init(secret_key);
+			String hash = Base64.encodeBase64String(sha256_HMAC.doFinal(payload.getBytes()));
+			return hash;
+		}catch (Exception e){
+			System.out.println("Error");
+			return null;
+		}
 	}
 }
