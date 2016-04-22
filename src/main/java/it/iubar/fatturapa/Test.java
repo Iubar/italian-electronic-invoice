926383138c5deddca55973091729427a32918f00
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2016-04-22 16:44:03 +0200

    Just a little Test

--- src/main/java/it/iubar/fatturapa/Test.java
+++ src/main/java/it/iubar/fatturapa/Test.java
@@ -1,9 +1,22 @@
 package it.iubar.fatturapa;
 
+import it.iubar.fatturapa.exceptions.AuthException;
+import it.iubar.fatturapa.exceptions.ParseNonriuscito;
+import it.iubar.fatturapa.exceptions.XmlNotvalid;
+import org.w3c.dom.Document;
+
 public class Test {
 
 	public static void main(String[] args) throws Exception{
 		FatturaUtil.setUser("andrea@iubar.it");
-		System.out.println(FatturaUtil.getFattura("1"));
+		try {
+			Document d = FatturaUtil.getFattura("1");
+		} catch (AuthException e) {
+			e.printStackTrace();
+		} catch (ParseNonriuscito parseNonriuscito) {
+			parseNonriuscito.printStackTrace();
+		} catch (XmlNotvalid xmlNotvalid) {
+			xmlNotvalid.printStackTrace();
+		}
 	}
 }
