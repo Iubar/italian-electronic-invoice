27f09b23f94d75f983f2c0ab6b76408544703a48
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2016-04-29 16:58:36 +0200

    Utility class changed
    Added documentation
    removed final to XSD_SCHEMA added setXsdUrl
    changed isValid method from public to private

--- src/main/java/it/iubar/fatturapa/FatturaUtil.java
+++ src/main/java/it/iubar/fatturapa/FatturaUtil.java
@@ -30,10 +30,22 @@ import java.util.Date;
 import java.util.Properties;
 
 public class FatturaUtil {
-	
+
+	/**
+	 * Tag for JSON data response
+	 */
 	public static final String DATA_TAG = "data";
+	/**
+	 * Tag for JSON server code response
+	 */
 	public static final String CODE_TAG = "code";
+	/**
+	 * Tag for JSON server message response
+	 */
 	public static final String RESPONSE_TAG = "response";
+	/**
+	 * Tag for JSON XML request
+	 */
 	public static final String XML_TAG = "xml";
 	
 	private static final String USER_PARAM = "user";
@@ -47,22 +59,33 @@ public class FatturaUtil {
 	private static String user = "user@user.it";
 
 	private static final String DEST_URL = "http://www.fatturatutto.it/app/api/test/";
-	private static final String XSD_SCHEMA = "http://www.fatturatutto.it/app/public/resources/xml/1.1/fatturapa_v1.1.xsd";
-
+	private static String XSD_SCHEMA = "http://www.fatturatutto.it/app/public/resources/xml/1.1/fatturapa_v1.1.xsd";
 	private static final String GET_FATTURA = "fattura-esempio/";
 	
 	private static String getUser() {
 		return user;
 	}
 
+	/**
+	 * Set the username for the request to the server. Default is <code>user@user.it</code>.
+	 * @param u Username
+     */
 	public static void setUser(String u) {
-		u = user;
+		user = u;
+	}
+
+	/**
+	 * Set the url where the parse model XSD is located
+	 * @param url destination url
+     */
+	public static void setXsdUrl(String url) {
+		XSD_SCHEMA = url;
 	}
 	
 	private static String getApi(){
 		return API_KEY;
 	}
-	
+
 	private static String setApi(){ // this will simply gets api key from file ini
 		Properties prop = new Properties();
 		InputStream input = null;
@@ -85,6 +108,15 @@ public class FatturaUtil {
 		}
 	}
 
+	/**
+	 * Returns the formatted XML {@link Document} of the given {@link String} invoice code.
+	 *
+	 * @param numeroFattura the invoice number called for download
+	 * @return the XML doc aleready validated
+	 * @throws AuthException the web server rejected your data. User, password or numeroFattura not valid
+	 * @throws ParseNonriuscito the invoice got from the server is not well-formatted as xsd
+	 * @throws XmlNotvalid the invoice got from the server is not complete or bad formatted
+	 */
 	public static Document getFattura(String numeroFattura) throws AuthException, ParseNonriuscito, XmlNotvalid {
 		JSONObject jsonObject = new JSONObject(callToHost(DEST_URL + GET_FATTURA + numeroFattura));
 		JSONObject data = jsonObject.getJSONObject(DATA_TAG);
@@ -142,7 +174,7 @@ public class FatturaUtil {
 		}
 	}
 
-	public static void isValid(Document document) throws XmlNotvalid {
+	private static void isValid(Document document) throws XmlNotvalid {
 		SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 
 		Source source = new StreamSource(XSD_SCHEMA);
@@ -164,8 +196,20 @@ public class FatturaUtil {
 
 	}
 
+	/**
+	 * Saves a xml file where you specify with savePath parameter.
+	 * Call the invoice by code using numeroFattura.
+	 *
+	 * @param savePath where you want to save the file
+	 * @param numeroFattura what invoice do you want
+	 * @throws TransformerException file not saved, probably the savePath is wrong
+	 * @throws AuthException the web server rejected your data. User, password or numeroFattura not valid
+	 * @throws ParseNonriuscito the invoice got from the server is not well-formatted as xsd
+	 * @throws XmlNotvalid the invoice got from the server is not complete or bad formatted
+     */
+	public static void saveFile(String savePath, String numeroFattura) throws TransformerException, AuthException, XmlNotvalid, ParseNonriuscito {
 
-	public static void saveFile(String savePath, Document d) throws TransformerException {
+		Document d = d = FatturaUtil.getFattura(numeroFattura);
 		Transformer transformer = TransformerFactory.newInstance().newTransformer();
 		Result output = new StreamResult(new File(savePath));
 		Source input = new DOMSource(d);
